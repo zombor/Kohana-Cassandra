21b5a5f5b07675eade9e9bee4cdbab1cebd5cb7e
Jeremy Bush <contractfrombelow@gmail.com>
2010-02-25 10:36:56 -0600

    Fixing a syntax error

--- config/cassandra.php
+++ config/cassandra.php
@@ -4,7 +4,7 @@ $GLOBALS['THRIFT_ROOT'] = $config['thrift_base'] = '/usr/share/php/Thrift';
 
 $config['default'] = array
 (
-	'server' => '172.0.0.1'.
+	'server' => '172.0.0.1',
 	'port' => 9160,
 	'keyspace' => 'Kohana',
 )
